87ff7f250fc4b13a64b5023a0e82ef1d65a1b6f5
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-06-23 07:40:57 +0300

    Initial Commit

---
+++ SwiftUILinkToTheWeb/SwiftUILinkToTheWebApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILinkToTheWebApp.swift
+//  SwiftUILinkToTheWeb
+//
+//  Created by Ramill Ibragimov on 23.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILinkToTheWebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
